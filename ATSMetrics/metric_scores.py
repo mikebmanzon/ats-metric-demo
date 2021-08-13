8393d047f3d535f55f812cedc24030cf69f75ce5
manzonm <manzon.m@northeastern.edu>
2021-08-13 15:44:15 -0400

    test issue

--- ATSMetrics/metric_scores.py
+++ ATSMetrics/metric_scores.py
@@ -9,45 +9,46 @@ def sam_score(x):
 
 class Scorer:
     def __init__(self):
-        self.qa_generator = pipeline("question-generation")
-        self.lerc_predictor = lercPredictor()
+        # self.qa_generator = pipeline("question-generation")
+        # self.lerc_predictor = lercPredictor()
         self.qa_predictor = QAPredictor()
         self.nspredictor = NSPredictor('bert-base-cased')
     def sam_score(self, orig_sentences, simplified_sentences, question=None, answer=None):
-        orig_sentence, simplified_sentence = orig_sentences[0], simplified_sentences[0]
-        if not question or not answer:
-            try:
-                generated_questions = self.qa_generator(orig_sentence)
-            except:
-                raise ValueError
-            if len(generated_questions) == 0:
-                raise ValueError
-            question = generated_questions[0]['question']
-            answer = generated_questions[0]['answer']
+        # orig_sentence, simplified_sentence = orig_sentences[0], simplified_sentences[0]
+        # if not question or not answer:
+        #     try:
+        #         generated_questions = self.qa_generator(orig_sentence)
+        #     except:
+        #         raise ValueError
+        #     if len(generated_questions) == 0:
+        #         raise ValueError
+        #     question = generated_questions[0]['question']
+        #     answer = generated_questions[0]['answer']
 
-        org_pred = self.qa_predictor.predict({"context": orig_sentence,
-                                         "question": question,
-                                         "reference": answer})[0]
-        simp_pred = self.qa_predictor.predict({"context": simplified_sentence,
-                                         "question": question,
-                                         "reference": answer})[0]
-        org_pred_score = self.lerc_predictor.predict({"candidate": org_pred,
-                  "context": orig_sentence,
-                  "question": question,
-                  "reference": answer})
-        simp_pred_score = self.lerc_predictor.predict({"candidate": simp_pred,
-                                                      "context": orig_sentence,
-                                                      "question": question,
-                                                      "reference": answer})
-        if org_pred_score > 5:
-            org_pred_score = 5
-        elif org_pred_score < -5:
-            org_pred_score = -5
-        if simp_pred_score > 5:
-            simp_pred_score = 5
-        elif simp_pred_score < -5:
-            simp_pred_score = -5
-        return sam_score(simp_pred_score - org_pred_score), question, simp_pred, org_pred, simp_pred_score, org_pred_score
+        # org_pred = self.qa_predictor.predict({"context": orig_sentence,
+        #                                  "question": question,
+        #                                  "reference": answer})[0]
+        # simp_pred = self.qa_predictor.predict({"context": simplified_sentence,
+        #                                  "question": question,
+        #                                  "reference": answer})[0]
+        # org_pred_score = self.lerc_predictor.predict({"candidate": org_pred,
+        #           "context": orig_sentence,
+        #           "question": question,
+        #           "reference": answer})
+        # simp_pred_score = self.lerc_predictor.predict({"candidate": simp_pred,
+        #                                               "context": orig_sentence,
+        #                                               "question": question,
+        #                                               "reference": answer})
+        # if org_pred_score > 5:
+        #     org_pred_score = 5
+        # elif org_pred_score < -5:
+        #     org_pred_score = -5
+        # if simp_pred_score > 5:
+        #     simp_pred_score = 5
+        # elif simp_pred_score < -5:
+        #     simp_pred_score = -5
+        # return sam_score(simp_pred_score - org_pred_score), question, simp_pred, org_pred, simp_pred_score, org_pred_score
+        return 100
     # PURPOSE
     # Given a list of original sentences and a list of simplified sentences,
     # calculate and return the BERNICE score for document cohesion.
